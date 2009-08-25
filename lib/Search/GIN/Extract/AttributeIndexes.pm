cdf0300e0709ce78f7f81d397320c2c5b84e3f20
Kent Fredric <kentfredric@gmail.com>
2009-08-25 19:21:39 +1200

    abstract, tidy

--- lib/Search/GIN/Extract/AttributeIndexes.pm
+++ lib/Search/GIN/Extract/AttributeIndexes.pm
@@ -3,23 +3,23 @@ use warnings;
 
 package Search::GIN::Extract::AttributeIndexes;
 
+# ABSTRACT: Automatically collect index metadata from MooseX::AttributeIndexes consuming models.
+
 use Moose;
 use Scalar::Util qw(blessed reftype);
 use Carp;
 extends 'Search::GIN::Extract::Callback';
 use namespace::autoclean;
 
-has 'extract' => (
-  init_arg => '_extract',
-  default => sub {
-    my( $object, $self , @args ) = @_;
+has '+extract' => (
+  default  => sub {
+    my ( $object, $self, @args ) = @_;
     return {} unless blessed $object;
     return {} unless $object->can('does');
     return {} unless $object->does('MooseX::AttributeIndexes::Provider');
     my $result = $object->attribute_indexes;
-    if( reftype $result ne 'HASH' ){
-      Carp::croak("the method 'attribute_indexes' on the class " . $object->meta->name .
-        " Does not return an array ref.");
+    if ( reftype $result ne 'HASH' ) {
+      Carp::croak( "the method 'attribute_indexes' on the class " . $object->meta->name . " Does not return an array ref." );
       return {};
     }
     return $result;
