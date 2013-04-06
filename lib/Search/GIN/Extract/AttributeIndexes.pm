41265aeee52a52907f10cb2905664fc4958f4b61
Kent Fredric <kentfredric@gmail.com>
2013-04-06 17:33:10 +1300

    Build results of df5cf2f (on master)

--- lib/Search/GIN/Extract/AttributeIndexes.pm
+++ lib/Search/GIN/Extract/AttributeIndexes.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Search::GIN::Extract::AttributeIndexes::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Search::GIN::Extract::AttributeIndexes::VERSION = '1.0.1';
+  $Search::GIN::Extract::AttributeIndexes::VERSION = '1.0.2';
 }
 
 use Moose;
@@ -40,8 +40,8 @@ no Moose;
 __PACKAGE__->meta->make_immutable;
 1;
 
-
 __END__
+
 =pod
 
 =head1 NAME
@@ -50,7 +50,7 @@ Search::GIN::Extract::AttributeIndexes - Automatically collect index metadata fr
 
 =head1 VERSION
 
-version 1.0.1
+version 1.0.2
 
 =head1 SYNOPSIS
 
@@ -104,10 +104,9 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Kent Fredric.
+This software is copyright (c) 2013 by Kent Fredric.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
