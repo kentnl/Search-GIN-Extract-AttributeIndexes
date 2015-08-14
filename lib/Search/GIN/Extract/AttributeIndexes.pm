d601642f0a36b414197987e8d2b06ffd785e7ed2
Kent Fredric <kentfredric@gmail.com>
2015-08-15 01:13:32 +1200

    Build results of 27279f0 (on master)

--- lib/Search/GIN/Extract/AttributeIndexes.pm
+++ lib/Search/GIN/Extract/AttributeIndexes.pm
@@ -2,16 +2,65 @@ use strict;
 use warnings;
 
 package Search::GIN::Extract::AttributeIndexes;
-our $AUTHORITY = 'cpan:KENTNL';
-$Search::GIN::Extract::AttributeIndexes::VERSION = '1.0.3';
+
+our $VERSION = '2.000000';
+
 use Moose;
 
 # ABSTRACT: Automatically collect index metadata from MooseX::AttributeIndexes consuming models.
+
+our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
 use Scalar::Util qw(blessed reftype);
 use Carp;
 extends 'Search::GIN::Extract::Callback';
 use namespace::autoclean;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 has '+extract' => (
@@ -39,13 +88,15 @@ __END__
 
 =pod
 
+=encoding UTF-8
+
 =head1 NAME
 
 Search::GIN::Extract::AttributeIndexes - Automatically collect index metadata from MooseX::AttributeIndexes consuming models.
 
 =head1 VERSION
 
-version 1.0.3
+version 2.000000
 
 =head1 SYNOPSIS
 
@@ -99,7 +150,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by Kent Fredric.
+This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
--- xt/author/eol.t
+++ xt/author/eol.t
@@ -1,15 +1,16 @@
 use strict;
 use warnings;
 
-# this test was generated with Dist::Zilla::Plugin::EOLTests 0.18
+# this test was generated with Dist::Zilla::Plugin::Test::EOL 0.18
 
 use Test::More 0.88;
 use Test::EOL;
 
 my @files = (
     'lib/Search/GIN/Extract/AttributeIndexes.pm',
-    't/00-compile.t',
-    't/000-report-versions-tiny.t',
+    't/00-compile/lib_Search_GIN_Extract_AttributeIndexes_pm.t',
+    't/00-report-prereqs.dd',
+    't/00-report-prereqs.t',
     't/02-basic.t'
 );
 
