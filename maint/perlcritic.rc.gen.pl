52c3b9429c9d117ef18f4fa5286f0844be6c656b
Kent Fredric <kentfredric@gmail.com>
2015-08-15 01:28:18 +1200

    Appease spell checker

--- maint/perlcritic.rc.gen.pl
+++ maint/perlcritic.rc.gen.pl
@@ -18,7 +18,11 @@ use Path::Tiny qw(path);
 my $bundle = create_bundle('Example::Author::KENTNL');
 $bundle->configure;
 
-my @stopwords = (qw());
+my @stopwords = (
+  qw(
+    metadata Instantiations KiokuDB
+    )
+);
 for my $wordlist (@stopwords) {
   $bundle->add_or_append_policy_field( 'Documentation::PodSpelling' => ( 'stop_words' => $wordlist ) );
 }
